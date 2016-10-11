02a3576d1ae51f764a436e7fede0f0cbcc2cd1b7
tonyben1991 <tonyben1991@outlook.com>
2016-10-11 09:25:22 +0800

    Folder Added

--- Module/PerformanceTest_Tony.c
+++ Module/PerformanceTest_Tony.c
@@ -4,7 +4,7 @@
             > 电子邮箱: tonyben1991@outlook.com
             > 创建时间: 2016-10-10-09.00
 
-            > 函数功能: 测试策略的性能
+            > 模块功能: 测试策略的性能
 ****************************************************************************************/
 
 Params
@@ -14,20 +14,16 @@ Vars
 Begin
     if (0 == BarStatus)
     {
-        FileDelete("E:\\TBPlusTest.txt");
+        FileDelete("E:\\Workspace\\TBPlusTest.txt");
         Commentary(TimeToString(CurrentTime));  //打印策略开始回测时间
-        FileAppend("E:\\TBPlusTest.txt", TimeToString(CurrentTime));
-        FileAppend("E:\\TBPlusTest.txt", "\n");
+        FileAppend("E:\\Workspace\\TBPlusTest.txt", TimeToString(CurrentTime));
+        FileAppend("E:\\Workspace\\TBPlusTest.txt", "");
     }
 
     if (2 == BarStatus)
     {
         Commentary(TimeToString(CurrentTime));  //打印策略完成回测时间
-        FileAppend("E:\\TBPlusTest.txt", TimeToString(CurrentTime));
+        FileAppend("E:\\Workspace\\TBPlusTest.txt", TimeToString(CurrentTime));
     }
-
-
-
-    return("测试完成!");
 
 End
