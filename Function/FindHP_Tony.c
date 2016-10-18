0bb6c8338bffdb21d07d31d02c6e492bb1e12d26
tonyben1991 <tonyben1991@outlook.com>
2016-10-18 16:30:28 +0800

    Update

--- Function/FindHP_Tony.c
+++ Function/FindHP_Tony.c
@@ -1,14 +1,12 @@
 /*******************************************************************************
-            > 文件: FindTop_Tony
+            > 文件: FindHP_Tony
             > 作者: Tony Ben
             > 电子邮箱: tonyben1991@outlook.com
-            > 创建时间: 2016-09-28-09.10
-
-            > 函数功能: 查找最高顶点Index
+            > 创建时间:
+            > 函数功能:
 *******************************************************************************/
 
 Params
-    NumericSeries myValue;  //出入的数值
     Numeric Length;         //传入的周期
 
 Vars
@@ -19,21 +17,21 @@ Begin
     NumericArrayInsert(IndexArr, 0, 0);
     for i = 2 to Length
     {
-        if (myValue[i] > myValue[i - 1] && myValue[i] > myValue[i - 2]
-            && myValue[i] > myValue[i + 1] && myValue[i] > myValue[i + 2])
+        if (Max(Open[i], Close[i]) >= Max(Open[i - 1], Close[i - 1]) && Max(Open[i], Close[i]) >= Max(Open[i - 2], Close[i - 2])
+            && Max(Open[i], Close[i]) >= Max(Open[i + 1], Close[i + 1]) && Max(Open[i], Close[i]) >= Max(Open[i + 2], Close[i + 2]))
         {
-            NumericArrayInsert(IndexArr, GetNumericArraySize(IndexArr) - 1, i);
+            NumericArrayInsert(IndexArr, GetNumericArraySize(IndexArr) - 1, CurrentBar - i);
         }
     }
 
     maxIndex = IndexArr[0];
     for i = 1 to GetNumericArraySize(IndexArr) - 2
     {
-        if (myValue[IndexArr[i]] > myValue[maxIndex])
+        if (Max(Open[CurrentBar - IndexArr[i]], Close[CurrentBar - IndexArr[i]]) > Max(Open[CurrentBar - maxIndex], Close[CurrentBar - maxIndex]))
         {
             maxIndex = IndexArr[i];
         }
     }
 
-    return CurrentBar - maxIndex; //返回最高顶部的Bar序号
+    return maxIndex; //返回最高顶部的Bar序号
 End
---
+++ Function/FindLP_Tony.c
@@ -0,0 +1,39 @@
+/*******************************************************************************
+            > 文件: FindLP_Tony
+            > 作者: Tony Ben
+            > 电子邮箱: tonyben1991@outlook.com
+            > 创建时间:
+
+            > 函数功能:
+*******************************************************************************/
+
+Params
+    Numeric Length;         //传入的周期
+
+Vars
+    Numeric i;                  //循环计数
+    NumericArray IndexArr;      //保存满足条件的Bar距离当前Bar的距离
+    Numeric minIndex;           //保存最的最低底部的Bar距离当前Bar的距离
+
+Begin
+    NumericArrayInsert(IndexArr, 0, 0);
+    for i = 2 to Length
+    {
+        if (Min(Open[i], Close[i]) <= Min(Open[i - 1], Close[i - 1]) && Min(Open[i], Close[i]) <= Min(Open[i - 2], Close[i - 2])
+            && Min(Open[i], Close[i]) <= Min(Open[i + 1], Close[i + 1]) && Min(Open[i], Close[i]) <= Min(Open[i + 2], Close[i + 2]))
+        {
+            NumericArrayInsert(IndexArr, GetNumericArraySize(IndexArr) - 1, CurrentBar - i);
+        }
+    }
+
+    minIndex = IndexArr[0];
+    for i = 1 to GetNumericArraySize(IndexArr) - 2
+    {
+        if (Min(Open[CurrentBar - IndexArr[i]], Close[CurrentBar - IndexArr[i]]) < Min(Open[CurrentBar - minIndex], Close[CurrentBar - minIndex]))
+        {
+            minIndex = IndexArr[i];
+        }
+    }
+
+    return minIndex; //返回最低底部的Bar序号
+End
